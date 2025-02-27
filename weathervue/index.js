ddea167364481d3a6137002afa871dbe2cf5464e
Uma7679 <150547156+Uma7679@users.noreply.github.com>
2025-02-27 22:40:23 +0530

    Update index.js

--- weathervue/index.js
+++ weathervue/index.js
@@ -6,7 +6,7 @@ const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-    const APIKey = '1a125cabf864177751c0d5ea67cc3ad5';
+    const APIKey = 'api-key';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '')
